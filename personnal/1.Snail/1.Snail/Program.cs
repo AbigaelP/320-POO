79042c0ea7b20c23a2e5333f022997a7dea9c8b6
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2024-08-30 11:58:06 +0200

    exercice_snail_terminer

--- personnal/1.Snail/1.Snail/Program.cs
+++ personnal/1.Snail/1.Snail/Program.cs
@@ -2,26 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _1.Snail
 {
     internal class Program
     {
-        public static void SetCursorPosition();
-        static void Main(string[] args)
-            /*    pseudo-code
-            Faire aparaitre
-            vie = 50
-            for /while      afficher à la bonne place
-                            vie --
-            Afficher mort
-            */
-        {
-            string snail = "_@_ö";
-            int life = 50;
 
-            
+        static void Main(string[] args)
+
+        {
+            //création d'une nouvelle liste
+            List<Snail> snails = new List<Snail>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                snails.Add(new Snail(i));
+            }
+
+            Console.CursorVisible = false;
+
+            while (snails[0].life > 0)
+            {
+                Console.Clear();
+                foreach (Snail snail in snails)
+                {
+                    Console.SetCursorPosition(snail.x, snail.y);
+                    Console.Write(snail.alive);
+                    snail.Move();
+
+                }
+                Thread.Sleep(40);
+            }
+
+            foreach (Snail snail in snails)
+            {
+                Console.SetCursorPosition(snail.x, snail.y);
+                Console.Write(snail.dead);
+            }
+
+            Console.ReadLine();
         }
 
     }
---
+++ personnal/1.Snail/1.Snail/Snail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Snail
+{
+    internal class Snail
+    {
+        public int life = 50;               //point de vie
+        public int x = 0;                   //position de l'axe x
+        public int y = 0;                   //position de l'axe y
+        public string alive = "_@_ö";       //visuel de l'escargot vivant
+        public string dead = "____";        //visuel de l'escargot mort
+        public string name;                 //nom de l'escargot
+
+        //Constructeur
+        public Snail(int y)
+        {
+            this.y = y;
+            this.name = "escargot" + y;
+        }
+
+        public Snail()
+        {
+            y = 12;
+            this.name = "Escargot";
+        }
+
+        //Méthodes
+        public void Move()
+        {
+            x++;
+            life--;      //un déplacement coûte un point de vie
+        }
+
+    }
+}
