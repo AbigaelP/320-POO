0f2a296a86900f9dfeba395941df80c9f572bd91
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2024-08-30 10:16:22 +0200

    exercice_snail-v1

---
+++ personnal/1.Snail/1.Snail/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Snail
+{
+    internal class Program
+    {
+        public static void SetCursorPosition();
+        static void Main(string[] args)
+            /*    pseudo-code
+            Faire aparaitre
+            vie = 50
+            for /while      afficher à la bonne place
+                            vie --
+            Afficher mort
+            */
+        {
+            string snail = "_@_ö";
+            int life = 50;
+
+            
+        }
+
+    }
+}
