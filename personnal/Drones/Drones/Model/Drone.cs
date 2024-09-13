42e135edf82b982b652be922b2b66d7821c7ba53
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2024-09-13 11:18:04 +0200

    refactor(drone): méthode statique de génération aléatoire

--- personnal/Drones/Drones/Model/Drone.cs
+++ personnal/Drones/Drones/Model/Drone.cs
@@ -1,11 +1,10 @@
-﻿namespace Drones
+﻿using Drones.Helpers;
+namespace Drones
 {
     // Cette partie de la classe Drone définit ce qu'est un drone par un modèle numérique
     public partial class Drone
     {
-        Random alea = new Random();
-
-        private int _charge = 1000;                   // La charge actuelle de la batterie
+        private int _charge = 1000;                     // La charge actuelle de la batterie
         private string _name;                           // Un nom
         private int _x ;                                // Position en X depuis la gauche de l'espace aérien
         private int _y;                                 // Position en Y depuis le haut de l'espace aérien
@@ -21,7 +20,7 @@
         public void Update(int interval)
         {
             X += 2;                                    // Il s'est déplacé de 2 pixels vers la droite
-            Y += alea.Next(-2, 3);                     // Il s'est déplacé d'une valeur aléatoire vers le haut ou le bas
+            Y += Helper.Random(-2, 3);                     // Il s'est déplacé d'une valeur aléatoire vers le haut ou le bas
             _charge--;                                  // Il a dépensé de l'énergie
         }
 
