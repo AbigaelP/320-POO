b2a94a0bcee293c7c926beca0e3238de8855065b
Abigaël Périsset <py81rbs@eduvaud.ch>
2024-10-04 10:34:48 +0200

    feat(Box): implémentation de la class Box

---
+++ personnal/Drones/Drones/Model/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Drones.Helpers;
+
+namespace Drones
+{
+    public enum SmartiesColor
+    {
+        Inconnu,
+        Rouge,
+        Jaune,
+        Brun,
+        Orange
+    }
+    public class Box
+    {
+        static int lastNumber;
+        private int _number;
+        private int _kilo = Helper.Random(5, 11);
+        private SmartiesColor _color = SmartiesColor.Inconnu;
+
+        public int Kilo { get => _kilo; }
+        public int Number { get => _number; }
+
+        public Box()
+        {
+            _number = lastNumber + 1;
+            lastNumber++;
+        }
+    }
+}
