940f70d28458466117f3983498134dcfde253a18
Abigaël Périsset <py81rbs@eduvaud.ch>
2024-10-04 10:37:06 +0200

    feat(Factory): différencie les Usines par un Id

--- personnal/Drones/Drones/Model/Factory.cs
+++ personnal/Drones/Drones/Model/Factory.cs
@@ -9,12 +9,16 @@ namespace Drones
     public partial class Factory : Building
     {
         private float _powerConsumption = 17F;
-
+        static int lastId;
+        private int _id;
+        private int _time = Helper.Random(2, 11);
         public float PowerConsumption { get => _powerConsumption; set => _powerConsumption = value; }
+        public int Id { get => _id; }
 
         public Factory()
         {
-            
+            _id = lastId + 1;
+            lastId++;
             Console.WriteLine(_powerConsumption);
         }
     }
