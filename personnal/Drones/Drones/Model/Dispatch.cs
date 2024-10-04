291d276f0c1384d02cf0b1cee20d8bea9d05ec39
Abigaël Périsset <py81rbs@eduvaud.ch>
2024-10-04 11:41:11 +0200

    feat(Dispatch): implémentation de l'interface IDispatchable

--- personnal/Drones/Drones/Model/Dispatch.cs
+++ personnal/Drones/Drones/Model/Dispatch.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Drones
 {
-    public class Dispatch
+    public class Dispatch : IDispatchable
     {
+        List<Box> boxes = new List<Box>();
+
+        public void Delete(Box soldBox)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Box newBox)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
